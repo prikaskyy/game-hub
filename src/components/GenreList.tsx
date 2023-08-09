89e24f420c7973f0e5fbb54fb8c4d73aea5aecf5
Martin Prikasky <prikasky.martin@gmail.com>
2023-08-09 13:28:52 +0200

    showing spinner while fetching genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,21 +1,24 @@
 import useGenre from "../hooks/useGenre";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/imge-url";
 
 const GenreList = () => {
-  const { data } = useGenre();
+  const { data, isLoading, error } = useGenre();
+
+  if (error) return null;
+  if (isLoading) return <Spinner />;
 
   return (
     <List>
       {data.map((genre) => (
-        <ListItem key={genre.id}>
-          <HStack paddingY={1}>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
             <Image
               src={getCroppedImageUrl(genre.image_background)}
               boxSize="32px"
               borderRadius={8}
             />
-            <Text>{genre.name}</Text>
+            <Text fontSize="lg">{genre.name}</Text>
           </HStack>
         </ListItem>
       ))}
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL:'https://api.rawg.io/api',
     params:{
-        key:'ee1078b84d694e5ba60229c826c6ae47'
+        key:'ee1078b84d694e5ba60229c826c6ae47x'
     }
 })
