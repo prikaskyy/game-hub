257ffccbc43022d93d4b8e47fdd8717940217db2
Martin Prikasky <prikasky.martin@gmail.com>
2023-08-09 12:06:44 +0200

    fetching genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import useGenre from "../hooks/useGenre";
+
+const GenreList = () => {
+  const { genre } = useGenre();
+
+  return (
+    <ul>
+      {genre.map((genre) => (
+        <li key={genre.id}>{genre.name}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default GenreList;
